d215c430c80b15ecfcf2cf357d85ad8d06ade32c
ishgem007 <ishgem007@gmail.com>
2021-12-20 10:55:11 +0000

    add project id

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "playground-s-11-ae139ac8-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="playground-s-11-ae139ac8"
